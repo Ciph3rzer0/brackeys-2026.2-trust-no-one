6473df8127d2c9f1d99cadbfa8974a5809bfc3ab
Josh White <ciph3rzer0@gmail.com>
2026-08-23 21:31:24 -0400

    feat: basic text filtering

--- ui/table/table_ui.gd
+++ ui/table/table_ui.gd
@@ -2,6 +2,7 @@ class_name TableUI
 extends PanelContainer
 
 var _database: Database
+var _rows: Array[TableRow]
 
 func _ready() -> void:
 	_database = Database.new()
@@ -12,10 +13,14 @@ func _ready() -> void:
 func set_table_items(database: Database):
 	_database = database
 	
-	for i in range(database.rows.size()):
-		var new_source_row: DatabaseRow = database.rows[i]
+	for new_source_row in database.rows:
 		var new_table_row: TableRow = %TableRow.duplicate()
 		new_table_row.set_source_row(new_source_row)
 		new_table_row.visible = true
+		_rows.append(new_table_row)
 		%TableBody.add_child(new_table_row)
-	
+
+
+func _on_plate_filter_filter_changed(text: String) -> void:
+	for row in _rows:
+		row.visible = !text or row.source_row.car_plate.containsn(text)
---
+++ ui/table/text_edit_filter.gd
@@ -0,0 +1,11 @@
+class_name TextEditFilter
+extends TextEdit
+
+signal filter_changed(text: String)
+
+func _ready() -> void:
+	text_changed.connect(_on_text_changed)
+	text_set.connect(_on_text_changed)
+
+func _on_text_changed() -> void:
+	filter_changed.emit(text)
