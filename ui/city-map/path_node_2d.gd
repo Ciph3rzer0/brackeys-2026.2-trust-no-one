898dc76c8f2c241785ed7aeec6e584ea0a286de0
Josh White <ciph3rzer0@gmail.com>
2026-08-27 23:25:50 -0400

    feat: many more work, residence nodes

--- ui/city-map/path_node_2d.gd
+++ ui/city-map/path_node_2d.gd
@@ -6,6 +6,8 @@ const BLACK_GRADIENT_TEXTURE_2D = preload("uid://c4cdvmetoww61")
 const GREEN_GRADIENT_TEXTURE_2D = preload("uid://qlv27q81cyvk")
 const ORANGE_GRADIENT_TEXTURE_2D = preload("uid://cp6p7x7g37eh6")
 const PINK_GRADIENT_TEXTURE_2D = preload("uid://c3w7gfq11xjbp")
+const HOME_GRADIENT_TEXTURE_2D = preload("uid://y08x8cseub27")
+const WORK_GRADIENT_TEXTURE_2D = preload("uid://cmlv71m2o7jy4")
 
 enum NodeType {
 	NULL = 0,
@@ -13,6 +15,8 @@ enum NodeType {
 	POI,
 	CityExit,
 	ShadyPOI,
+	Residence,
+	Work,
 }
 
 @export var connections: Array[NodePath] = []
@@ -28,3 +32,7 @@ enum NodeType {
 				texture = ORANGE_GRADIENT_TEXTURE_2D
 			NodeType.ShadyPOI:
 				texture = PINK_GRADIENT_TEXTURE_2D
+			NodeType.Residence:
+				texture = HOME_GRADIENT_TEXTURE_2D
+			NodeType.Work:
+				texture = WORK_GRADIENT_TEXTURE_2D
