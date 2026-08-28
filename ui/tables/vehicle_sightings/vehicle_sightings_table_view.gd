9eb9c98767697a76a460c0d9cb878a8512b60456
Josh White <ciph3rzer0@gmail.com>
2026-08-28 13:35:45 -0400

    feat: option filters for color, type, and features

--- ui/tables/vehicle_sightings/vehicle_sightings_table_view.gd
+++ ui/tables/vehicle_sightings/vehicle_sightings_table_view.gd
@@ -26,6 +26,9 @@ func clear_table_rows():
 
 var plate_filter := ''
 var camera_filter := ''
+var color_filter := ''
+var type_filter := ''
+var features_filter := ''
 
 func _filter_rows():
 	for row in _rows:
@@ -34,6 +37,18 @@ func _filter_rows():
 			continue
 			
 		if camera_filter.length() and !camera_filter.is_subsequence_ofn(row.source_row.camera_id):
+			row.visible = false
+			continue
+			
+		if color_filter.length() and !color_filter.is_subsequence_ofn(row.source_row.vehicle_color):
+			row.visible = false
+			continue
+			
+		if type_filter.length() and !type_filter.is_subsequence_ofn(row.source_row.vehicle_type):
+			row.visible = false
+			continue
+			
+		if features_filter.length() and !features_filter.is_subsequence_ofn(", ".join(row.source_row.vehicle_features)):
 			row.visible = false
 			continue
 		
@@ -45,4 +60,16 @@ func _on_plate_filter_filter_changed(text: String) -> void:
 
 func _on_camera_filter_filter_changed(text: String) -> void:
 	camera_filter = text
+	_filter_rows()
+
+func _on_color_filter_filter_changed(text: String) -> void:
+	color_filter = text
+	_filter_rows()
+
+func _on_type_filter_filter_changed(text: String) -> void:
+	type_filter = text
+	_filter_rows()
+
+func _on_features_filter_filter_changed(text: String) -> void:
+	features_filter = text
 	_filter_rows()
