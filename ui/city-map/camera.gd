11da42c8a3f62eb9f46489a702ddcd28b482813d
Josh White <ciph3rzer0@gmail.com>
2026-08-27 21:59:39 -0400

    feat: date offset and simulation time warp field

--- ui/city-map/camera.gd
+++ ui/city-map/camera.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+@export var city_map: CityMap
 @export var camera_name: String
 
 func _ready() -> void:
 	assert(camera_name)
+	assert(city_map)
 
 
 func _on_body_entered(vehicle: Node2D) -> void:
@@ -19,11 +21,11 @@ func log_car_sighting(vehicle: Vehicle):
 	print("Camera ", camera_name, " sees ", vehicle.name)
 	var sighting = SchemaVehicleSightings.new()
 	#sighting.sighting_id: int
-	sighting.unix_timestamp = Time.get_unix_time_from_system()
+	sighting.unix_timestamp = city_map.current_time
 	sighting.camera_id = camera_name
 	sighting.vehicle_plate = vehicle.plate
 	sighting.vehicle_color = vehicle.color
 	sighting.vehicle_type = vehicle.type
 	sighting.vehicle_features = vehicle.features
 	GameManager.database.vehicle_sightings.append(sighting)
-	#GameManager.database.refresh()
+	GameManager.database.refresh()
--- ui/city-map/test_drive.gd
+++ ui/city-map/test_drive.gd
@@ -11,28 +11,41 @@ extends Node2D
 @export var end_marker: Node2D
 @export var vehicle: Vehicle
 
+@export var time_warp_factor := 60*60 * 0.5
+@export var start_time_string: String
+@export_storage var start_time: int
+
 var astar: AStar2D
+var current_time: float
+
+func _physics_process(delta: float) -> void:
+	current_time += delta * time_warp_factor
+	$TimeLabel.text = DateHelper.time_12h(current_time)
 
 func _ready() -> void:
 	assert(astar_graph_2d)
 	assert(vehicle)
 	assert(start_marker)
 	assert(end_marker)
-
+	
 	astar = astar_graph_2d.build_astar()
-
+	
 	var start_node := astar_graph_2d.find_closest_point(start_marker.global_position)
 	var end_node := astar_graph_2d.find_closest_point(end_marker.global_position)
 	assert(start_node and end_node, "astar_graph_2d has no PathNode2D children")
-
+	
 	var from_id := astar_graph_2d.get_id_for(start_node)
 	var to_id := astar_graph_2d.get_id_for(end_node)
-
+	
 	var path := astar.get_point_path(from_id, to_id)
 	if path.is_empty():
 		push_warning("No path found between start_marker and end_marker.")
-
+	
 	vehicle.follow_path(path)
+	
+	start_time = Time.get_unix_time_from_datetime_string(start_time_string)
+	assert(start_time > 0)
+	current_time = start_time
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
