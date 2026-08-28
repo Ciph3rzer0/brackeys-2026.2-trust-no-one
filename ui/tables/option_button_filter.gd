be0504809f0df709fdd2acf8f881bfe39ba537e3
Josh White <ciph3rzer0@gmail.com>
2026-08-28 15:07:17 -0400

    feat: date filter

--- ui/tables/option_button_filter.gd
+++ ui/tables/option_button_filter.gd
@@ -9,6 +9,4 @@ func _ready() -> void:
 	item_selected.connect(_on_item_selected)
 
 func _on_item_selected(i: int) -> void:
-	print(i, " ", selected)
-	print(get_item_text(i), " ", get_item_text(selected))
 	filter_changed.emit(get_item_text(i))
--- ui/tables/vehicle_sightings/vehicle_sightings_table_view.gd
+++ ui/tables/vehicle_sightings/vehicle_sightings_table_view.gd
@@ -30,8 +30,55 @@ var color_filter := ''
 var type_filter := ''
 var features_filter := ''
 
+var date_filter := ''
+var time_filter := ''
+var range_filter := ''
+
 func _filter_rows():
+	var start := -1
+	var end := -1
+	
+	if date_filter.length() <= 2 and date_filter.is_valid_int():
+		var date = "2026-08-" + date_filter
+		var time_regex = RegEx.new()
+		var range_regex = RegEx.new()
+		
+		# Double backslash is required so GDScript passes '\d' to the engine
+		time_regex.compile("(\\d{1,2}):(\\d{2})") 
+		var time_r := time_regex.search(time_filter)
+		if time_r:
+			var hour = time_r.get_string(1).pad_zeros(2)
+			var minute  = time_r.get_string(2)
+			date += "T"+hour+":"+minute+":00"
+		
+		start = Time.get_unix_time_from_datetime_string(date)
+				
+		# Determine range
+		range_regex.compile("([+-]?)(\\d{1,2})([hm])") 
+		var range_r := range_regex.search(range_filter)
+		if range_r:
+			# Individual capture groups
+			var sign_part = range_r.get_string(1)  # Yields "-"
+			var num_part  = range_r.get_string(2)  # Yields "45"
+			var unit_part = range_r.get_string(3)  # Yields "m"
+			
+			# Example use case: Convert text to a clean integer
+			var second_range = num_part.to_int() * 60
+			
+			if unit_part == "h":
+				second_range *= 60
+			
+			end = start + second_range
+		
+	#remaining filters
 	for row in _rows:
+		if row.source_row.unix_timestamp < start:
+			row.visible = false
+			continue
+		if end > 0 and row.source_row.unix_timestamp > end:
+			row.visible = false
+			continue
+		
 		if plate_filter.length() and !plate_filter.is_subsequence_ofn(row.source_row.vehicle_plate):
 			row.visible = false
 			continue
@@ -52,8 +99,14 @@ func _filter_rows():
 			row.visible = false
 			continue
 		
+		
+		if features_filter.length() and !features_filter.is_subsequence_ofn(", ".join(row.source_row.vehicle_features)):
+			row.visible = false
+			continue
+		
 		row.visible = true
 
+#region Filter signal hooks
 func _on_plate_filter_filter_changed(text: String) -> void:
 	plate_filter = text
 	_filter_rows()
@@ -73,3 +126,16 @@ func _on_type_filter_filter_changed(text: String) -> void:
 func _on_features_filter_filter_changed(text: String) -> void:
 	features_filter = text
 	_filter_rows()
+
+func _on_date_filter_filter_changed(text: String) -> void:
+	date_filter = text
+	_filter_rows()
+
+func _on_time_filter_filter_changed(text: String) -> void:
+	time_filter = text
+	_filter_rows()
+
+func _on_time_range_filter_filter_changed(text: String) -> void:
+	range_filter = text
+	_filter_rows()
+#endregion
